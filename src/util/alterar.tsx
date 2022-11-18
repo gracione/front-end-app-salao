82fb32f58aa754b619939bbe380ccda93a017c14
ipeweb <gracione2017@gmail.com>
2022-11-18 11:21:49 -0300

    alterado

--- src/util/alterar.tsx
+++ src/util/alterar.tsx
@@ -15,7 +15,7 @@ export default function Alterar({ modulo, dados }: any) {
       <Modal open={open} onClose={() => setOpen(false)}>
         <div className='modal'>
           <img src="/icons/salvo.png" alt="" />
-          <h2>Item salvo com sucesso!</h2>
+          <h2>Item alterado com sucesso!</h2>
           <h2><a href={"/" + modulo}>ok</a></h2>
         </div>
       </Modal>
