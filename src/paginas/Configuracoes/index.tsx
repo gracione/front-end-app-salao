ab4e0a1f285bb4e254e926fed871f063b9baace8
gracione <gracione2017@gmail.com>
2022-11-19 14:14:34 -0300

    atualizando

--- src/paginas/Configuracoes/index.tsx
+++ src/paginas/Configuracoes/index.tsx
@@ -1,12 +1,60 @@
+import api from '../../services/api';
 import { Conteudo, Header } from "../../styles/global";
+import { useState, useEffect } from "react";
+import Alterar from "../../util/alterar";
 
 export default function Configuracoes() {
+  const [nome, setNome] = useState('');
+  const [numero, setNumero] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  
+  const [listagem, setListagem]: any = useState([]);
 
+  useEffect(() => {
+    api
+      .post("/configuracao/listar-id", {
+        id: localStorage.getItem('token')
+      })  
+      .then((response) => setListagem(response.data));
+
+  }, []);
+
+  console.log(listagem);
   return (
-    <Header>
-      <Conteudo>
-        <h1>Folgas</h1>
-      </Conteudo>
-    </Header>
+    <Conteudo>
+      <h1>Configuração</h1>
+
+      <label>Nome</label>
+      <input
+        defaultValue={listagem.nome}
+        onChange={e => setNome(e.target.value)}
+        required
+      />
+
+      <label>Contato(telefone)</label>
+      <input
+        defaultValue={listagem.numero}
+        onChange={e => setNumero(e.target.value)}
+        required
+      />
+      <label>Email</label>
+      <input
+        defaultValue={listagem.email}
+        onChange={e => setEmail(e.target.value)}
+        required
+      />
+      <label>Senha</label>
+      <input
+        onChange={e => setSenha(e.target.value)}
+        required
+      />
+      <Alterar
+          modulo="configuracoes"
+          dados={{
+              id: listagem.id, nome, numero, senha, email}}
+        />
+
+    </Conteudo>
   );
 }
