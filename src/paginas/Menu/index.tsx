d3f370ac0633221da6803d39b561ed99c392cb7a
gracione <gracione2017@gmail.com>
2022-11-30 07:26:16 -0300

    atualizar codigo

--- src/paginas/Menu/index.tsx
+++ src/paginas/Menu/index.tsx
@@ -4,14 +4,17 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import BuscarDadosApi from "../../util/util";
 
 export default function Menu() {
   const history = useNavigate();
+
+  const servicos = BuscarDadosApi('servicos', 'listar', { idUsuario:localStorage.getItem('id_usuario') });
+
+  console.log(servicos);
   function logout() {
     localStorage.clear();
     window.location.href = "/login";
-
   }
 
   return (
@@ -19,13 +22,18 @@ export default function Menu() {
       <Container >
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" >
-        <Navbar.Brand href="#home">
-          <img width="40px" onClick={() => history("/home")} src="/logo-alternativa.svg" alt="logo Cyan" />
-        </Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img width="40px" onClick={() => history("/home")} src="/logo-alternativa.svg" alt="logo Cyan" />
+          </Navbar.Brand>
           <Nav className="me-auto d-flex justify-content-around w-75" >
             <Nav.Link>
               <Link to="/home">Home</Link>
             </Nav.Link>
+            {servicos.map((element) => (
+              <Nav.Link>
+                <Link to="/funcionarios">Funcionários</Link>
+              </Nav.Link>
+            ))}
             <Nav.Link>
               <Link to="/funcionarios">Funcionários</Link>
             </Nav.Link>
@@ -45,7 +53,7 @@ export default function Menu() {
               <Link to="/profissao">Profissões</Link>
             </Nav.Link>
             <Nav.Link>
-                <Link to="/configuracoes">Configurações</Link>
+              <Link to="/configuracoes">Configurações</Link>
             </Nav.Link>
             <Nav.Link>
               <NavDropdown.Item onClick={logout} >Sair</NavDropdown.Item>
