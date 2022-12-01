175ffa79d25dfbbe81cf0adbc505521f44334fb3
gracione <gracione2017@gmail.com>
2022-12-01 07:27:57 -0300

    atualizando dados

--- src/paginas/Menu/index.tsx
+++ src/paginas/Menu/index.tsx
@@ -18,32 +18,32 @@ export default function Menu() {
   }
 
   return (
-    <Navbar className="navbar navbar-dark bg-primary" expand="lg">
-      <Container >
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" >
-          <Navbar.Brand href="#home">
+    <Navbar className="navbar navbar-dark bg-primary d-flex justify-content-between w-100" expand="lg">
+          <Navbar.Brand href="#home" className="border" >
             <img width="40px" onClick={() => history("/home")} src="/logo-alternativa.svg" alt="logo Cyan" />
           </Navbar.Brand>
-          <Nav className="me-auto d-flex justify-content-around w-75" >
-            <Nav.Link>
-              <Link to="/home">Home</Link>
-            </Nav.Link>
-            {servicos.map((element) => (
-              <Nav.Link>
-                <Link to={"/" + element.url}>{element.nome}</Link>
-              </Nav.Link>
-            ))}
-            <Nav.Link>
-              <Link to="/configuracoes">Configurações</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <NavDropdown.Item onClick={logout} >Sair</NavDropdown.Item>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="w-25 border"  />
+          <Navbar.Collapse id="basic-navbar-nav"  className="bg-info border" >
+            <div className="d-flex f-right bg-dark" >
+              <Nav className="" >
+                <Nav.Link>
+                  <Link to="/home">Home</Link>
+                </Nav.Link>
+                {servicos.map((element) => (
+                  <Nav.Link>
+                    <Link to={"/" + element.url}>{element.nome}</Link>
+                  </Nav.Link>
+                ))}
+                <Nav.Link>
+                  <Link to="/configuracoes">Configurações</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <NavDropdown.Item onClick={logout} >Sair</NavDropdown.Item>
+                </Nav.Link>
+              </Nav>
+            </div>
+          </Navbar.Collapse>
     </Navbar>
-
+    
   );
 }
