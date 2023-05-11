185750c60d9a67ac39088e13e8cb99e7caa27264
gracione <gracione2017@gmail.com>
2023-05-10 21:13:49 -0300

    padronizando

--- src/paginas/Autenticacao/Login/styles.ts
+++ src/paginas/Autenticacao/Login/styles.ts
@@ -1,31 +1,33 @@
-import styled from "styled-components"
+import styled from "styled-components";
+
+const backgroundUrl = "/fundo/fundo-login.png";
 
 export const Container = styled.div`
+  background: linear-gradient(rgba(0, 0, 0, 1.4), rgba(0, 0, 0, 0.4)),
+    url(${backgroundUrl});
+  background-size: 1500px 750px;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-position: center center;
 
-    @media (min-width: 768px) 
-    {
-        form {
-           width : 50% ;
-           height: 60vh ;
-        }
-        
-    }
-    @media (max-width: 767px) {
-        form {
-           width : 100% ;
-           height: 50vh ;
-        }
-    }
+  color: white;
+  padding: 3vh;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
-    background: linear-gradient(rgba(0,0,0,1.4), rgba(0,0,0,0.4)),
-    url('/fundo/fundo-login.png');
-    background-size: 1500px 750px;
-    background-attachment: fixed;
-    background-repeat: no-repeat;
-    color: white;
-    padding: 3vh;
-    height: 100vh ;
-    display: flex;
-    flex-direction: column ;
-    justify-content: space-between ;
+  @media (min-width: 768px) {
+    form {
+      width: 50%;
+      height: 60vh;
+    }
+  }
+
+  @media (max-width: 767px) {
+    form {
+      width: 100%;
+      height: 50vh;
+    }
+  }
 `;
