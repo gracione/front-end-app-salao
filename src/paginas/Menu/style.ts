771662de8665047ad6c28306279fa417b17c0fad
gracione <gracione2017@gmail.com>
2023-05-10 21:20:09 -0300

    padronizando

--- src/paginas/Menu/style.ts
+++ src/paginas/Menu/style.ts
@@ -1,58 +1,62 @@
 import styled from "styled-components";
 
+const logoHeightMobile = '100%';
+const logoHeightDesktop = '10vh';
+
 export const Container = styled.div`
-        display: flex;
-        justify-content: space-between;
-        width: 50px ;
-        text-transform: capitalize;
-        flex-direction: column ;
-        position: fixed ;
-                
-    @media (max-width: 767px) {
-        width: 100% ;
-        .desktop{
-            display: none ;
-        }
-        .logo{
-            margin:0 ;
-            padding:0    ;
-            height: 100% ;
-        }
-        .menu{
-            height: 100%;
-            width: 100% ;
-            padding: 0px ;
-        }
-        .menuB{
-            width: 70px;
-            height: 30px ;
-        }
+  display: flex;
+  justify-content: space-between;
+  width: 50px;
+  text-transform: capitalize;
+  flex-direction: column;
+  position: fixed;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    
+    .desktop {
+      display: none;
+    }
+    .logo {
+      margin: 0;
+      padding: 0;
+      height: ${logoHeightMobile};
+    }
+    .menu {
+      height: 100%;
+      width: 100%;
+      padding: 0px;
+    }
+    .menuB {
+      width: 70px;
+      height: 30px;
+    }
+  }
+
+  @media (min-width: 767px) {
+    .mobile {
+      display: none;
+    }
+    .logo {
+      margin: 0;
+      padding: 0;
+      height: ${logoHeightDesktop};
     }
 
-    @media (min-width: 767px) {
-        .mobile{
-            display: none ;
-        }
-        .logo{
-            margin:0 ;
-            padding:0    ;
-            height: 10vh ;
-        }
-
-        .desktop{
-            width: 100% ;
-            height: 80vh ;
-            display: flex ;
-            flex-direction: column ;
-        }
-        .menu{
-            display: flex ;
-            flex-direction: column ;
-            height: 100%;
-            width: 100% ;
-            padding: 0px ;
-        }
-
-        height: 100vh ;
+    .desktop {
+      width: 100%;
+      height: 80vh;
+      display: flex;
+      flex-direction: column;
     }
+    .menu {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      width: 100%;
+      padding: 0px;
+    }
+
+    height: 100vh;
+  }
 `;
