15777b593f9514dd4f10fe30e3a7755537b9379e
gracione <gracione2017@gmail.com>
2023-05-10 22:03:17 -0300

    padronizando

--- src/paginas/Home/index.tsx
+++ src/paginas/Home/index.tsx
@@ -1,41 +1,43 @@
-import { AgendarHorario, HorariosMarcados, Container } from "./styles";
-import HorariosMarcado from "./horariosMarcados";
-import Funcionarios from "../../components/funcionarios";
 import { useState } from 'react';
+import Funcionarios from '../../components/funcionarios';
+import HorariosMarcado from './horariosMarcados';
+import { AgendarHorario, Container, HorariosMarcados } from './styles';
 
 export default function Home() {
-  const idTipoUsuario = localStorage.getItem("tipo_usuario");
-  const [nomeCliente, SetNomeCliente] = useState('');
+  // Extrai o valor da chave "tipo_usuario" do localStorage
+  const { tipo_usuario } = localStorage;
+
+  // Define o estado "nomeCliente" para armazenar o nome do cliente
+  const [nomeCliente, setNomeCliente] = useState('');
 
   return (
     <Container>
-      <div className="home-usuario" >
+      {/* Área de agendamento de horários */}
+      <div className="home-usuario">
         <AgendarHorario>
-          <h6 className="bg-white w-100 border titulo d-flex justify-content-center" >Agendar Horário</h6>
-          <div className="w-100" >
-            {idTipoUsuario === '1' &&
-              <h5>Etapa Escolher Funcionário e Profissão</h5>
-            }
-            {idTipoUsuario === '2' &&
-              <h5>Etapa Escolher Profissão</h5>
-            }
-            {idTipoUsuario === '3' &&
-              <h5>Etapa Escolher Funcionário e Profissão</h5>
-            }
-            {idTipoUsuario !== '3' &&
-              <input type="text" onChange={e => SetNomeCliente(e.target.value)} placeholder="Digite o nome do cliente" required />
-            }
+          <h6 className="bg-white w-100 border titulo d-flex justify-content-center">Agendar Horário</h6>
+          <div className="w-100">
+            {/* Define a etapa adequada baseada no tipo de usuário */}
+            {tipo_usuario === '1' && <h5>Etapa Escolher Funcionário e Profissão</h5>}
+            {tipo_usuario === '2' && <h5>Etapa Escolher Profissão</h5>}
+            {tipo_usuario === '3' && <h5>Etapa Escolher Funcionário e Profissão</h5>}
+
+            {/* Solicita o nome do cliente caso o tipo de usuário não seja "3" */}
+            {tipo_usuario !== '3' && (
+              <input type="text" onChange={e => setNomeCliente(e.target.value)} placeholder="Digite o nome do cliente" required />
+            )}
             <hr />
           </div>
-          <Funcionarios
-            nomeCliente={nomeCliente}
-          /> 
+          {/* Renderiza a lista de funcionários */}
+          <Funcionarios nomeCliente={nomeCliente} />
         </AgendarHorario>
       </div>
-      
+
+      {/* Área de horários marcados */}
       <HorariosMarcados>
         <label>Horários Marcados</label>
-        <HorariosMarcado></HorariosMarcado>
+        {/* Renderiza a lista de horários marcados */}
+        <HorariosMarcado />
       </HorariosMarcados>
     </Container>
   );
