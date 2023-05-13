2ff9f70bf79b2ee4c2581fbff72965e7e1b9068f
gracione <gracione2017@gmail.com>
2023-05-13 12:21:47 -0300

    modiciando menu

--- src/paginas/Menu/index.tsx
+++ src/paginas/Menu/index.tsx
@@ -1,87 +1,74 @@
 import { Link } from "react-router-dom";
-import { Container } from "./style";
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { AiFillSetting, AiFillHome } from "react-icons/ai";
-import { BsFillCalendarDayFill } from "react-icons/bs";
-import { FaSignOutAlt, FaCalendarDay, FaUserPlus } from "react-icons/fa";
-import { GiHairStrands } from "react-icons/gi";
-import { RiBriefcase4Fill } from "react-icons/ri";
-import { BiTimeFive } from "react-icons/bi";
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faCog, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { GoogleLogout } from 'react-google-login';
 
-const iconLogout = <FaSignOutAlt />;
-const clientId = "959861611664-n7ql4k5hf128e48qbsspdhu0vdkd3sar.apps.googleusercontent.com";
-const perfil: any = ['nenhum', 'Administrativo', 'Funcionario', 'Cliente'];
-const tipoUsuario: any = localStorage.getItem("tipo_usuario");
+const Menu = () => {
+  const user = { name: 'Nome do Usuário', photo: 'url_da_foto' };
+  const isAdmin = true; // Defina como true ou false conforme necessário
+  const CLIENTE_ID = "959861611664-n7ql4k5hf128e48qbsspdhu0vdkd3sar.apps.googleusercontent.com";
+  const perfil: any = ['nenhum', 'Administrativo', 'Funcionario', 'Cliente'];
+  const tipoUsuario: any = localStorage.getItem("tipo_usuario");
+  
+  const handleLogout = () => {
+      localStorage.clear();
+      window.location.href = "/login";
+  };
 
-const logOut = () => {
-  localStorage.clear();
-  window.location.href = "/login";
-};
-
-export default function Menu(): any {
   return (
-    <Container>
-      <Navbar className="menu" collapseOnSelect expand="lg" bg="primary" variant="dark">
-        <Navbar.Brand className="logo" href="/home">
-          <img width="40px" src="/logo-alternativa.svg" alt="salao" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" className="menuB" />
-        <Navbar.Collapse id="responsive-navbar-nav bg-dark">
-          <Nav className="me-auto">
-          </Nav>
-          <Nav className="mobile">
-            <Link className="m-2" to="/home">Home</Link>
-            {tipoUsuario === '1' && (
+    <Navbar expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="/">Realize</Navbar.Brand>
+      <Navbar.Toggle aria-controls="menu-navbar" />
+      <Navbar.Collapse id="menu-navbar">
+        <Nav className="mr-auto">
+        {tipoUsuario === '1' && (
+          <>
+          <Nav.Link><Link to={"/funcionarios"}>Funcionario</Link></Nav.Link>
+          <Nav.Link><Link to={"/feriados"}>Feriados</Link></Nav.Link>
+          <Nav.Link><Link to={"/folgas"}>Folgas</Link></Nav.Link>
+          <Nav.Link><Link to={"/tratamentos"}>Procedimentos</Link></Nav.Link>
+          <Nav.Link><Link to={"/profissao"}>Profissão</Link></Nav.Link>
+          <Nav.Link><Link to={"/expediente"}>Expediente</Link></Nav.Link>
+          <NavDropdown title="Relatórios">
+            <NavDropdown.Item>
+              Relatório de Vendas
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              Relatório de Estoque
+            </NavDropdown.Item>
+          </NavDropdown>
+          </>
+        )}
+        </Nav>
+        <Nav className="ml-auto">
+          <NavDropdown
+            title={
               <>
-                <Link className="m-2" to={"/funcionarios"} >funcionários</Link>
-                <Link className="m-2" to={"/feriados"} >feriados</Link>
-                <Link className="m-2" to={"/folgas"} >folgas</Link>
-                <Link className="m-2" to={"/tratamentos"} >tratamentos</Link>
-                <Link className="m-2" to={"/profissao"} >profissão</Link>
+                <FontAwesomeIcon icon={faUser} /> {user.name}
               </>
-            )
             }
-            <Link className="m-2" to="/configuracoes">Configurações</Link>
-            <div className="m-2 text-info">
-              <div className="d-flex" >
-                <div>{perfil[tipoUsuario]}</div>
-                : &nbsp;
-                <div >{localStorage.getItem('nome')}</div>
-                </div>
-            </div>
-          </Nav>
-
-          <Nav className="desktop" >
-            <Link className="m-2" to="/home"><AiFillHome /></Link>
-            {tipoUsuario === '1' && (
-              <>
-                <Link className="m-2" to={"/funcionarios"} ><FaUserPlus /></Link>
-                <Link className="m-2" to={"/feriados"} ><FaCalendarDay /></Link>
-                <Link className="m-2" to={"/folgas"} ><BsFillCalendarDayFill /></Link>
-                <Link className="m-2" to={"/tratamentos"} ><GiHairStrands /></Link>
-                <Link className="m-2" to={"/profissao"} ><RiBriefcase4Fill /></Link>
-                <Link className="m-2" to={"/expediente"}><BiTimeFive /></Link>
-              </>
-            )
-            }
-            <Link className="m-2" to="/configuracoes"><AiFillSetting /></Link>
-            <div>
-              <GoogleLogout
-                clientId={clientId}
-                onLogoutSuccess={logOut}
+            id="user-dropdown"
+          >
+              <NavDropdown.Item href="/configuracoes">
+                <FontAwesomeIcon icon={faCog} /> Configurações
+              </NavDropdown.Item>
+            <NavDropdown.Item onClick={handleLogout}>
+            <GoogleLogout
+                clientId={CLIENTE_ID}
+                onLogoutSuccess={handleLogout}
                 render={(renderProps) => (
-                  <div className="btn text-info" onClick={renderProps.onClick}>
-                    <FaSignOutAlt />
+                  <div onClick={renderProps.onClick}>
+                    <FontAwesomeIcon icon={faSignOutAlt} /> Sair
                   </div>
                 )}
               />
-
-            </div>
-            </Nav>
-            </Navbar.Collapse>
-      </Navbar >
-    </Container>
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
-}
+};
+
+export default Menu;
