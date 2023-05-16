9c486f0b26caf470a7e829d57f4f88c23570595c
gracione <gracione2017@gmail.com>
2023-05-16 13:25:07 -0300

    update

--- src/util/inserir.tsx
+++ src/util/inserir.tsx
@@ -2,26 +2,28 @@ import { Modal } from 'react-responsive-modal';
 import React, { useState } from 'react';
 import api from '../../src/services/api';
 
-export default function Inserir({modulo, dados}: any) {
+export default function Inserir({ modulo, dados }: any) {
   const [open, setOpen] = useState(false);
-  function inserir(modulo: any, dados: any) {
-    let url: any = "/" + modulo + "/inserir";
-    api.post(url, dados).then((response) => (setOpen(response.data)));
+
+  function inserir(modulo: string, dados: any) {
+    const url = `/${modulo}/inserir`;
+    api.post(url, dados).then((response) => setOpen(response.data));
   }
 
   return (
     <div>
       <Modal open={open} onClose={() => setOpen(false)}>
-      <div className='modal'>
-          <div className='salvo'>
-          <img src="/icons/salvo.png" alt="" />
-          <h2>Item salvo com sucesso!</h2>
-          <h2><a href={"/" + modulo}>ok</a></h2>
-        </div>
+        <div className="modal">
+          <div className="salvo">
+            <img src="/icons/salvo.png" alt="" />
+            <h2>Item salvo com sucesso!</h2>
+            <h2>
+              <a href={`/${modulo}`}>ok</a>
+            </h2>
+          </div>
         </div>
       </Modal>
-      <button onClick={() => inserir(modulo, dados)} >Inserir</button>
+      <button onClick={() => inserir(modulo, dados)}>Inserir</button>
     </div>
   );
-
 }
--- src/util/listar.tsx
+++ src/util/listar.tsx
@@ -1,53 +1,28 @@
 import { Modal } from 'react-responsive-modal';
-import api from '../../src/services/api';
-import { Adicionar, Conteudo, Header, TituloFuncao } from '../styles/global';
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { FaTrashAlt } from "react-icons/fa";
 import { BsFillPencilFill } from "react-icons/bs";
+import api from '../../src/services/api';
+import { Adicionar, Conteudo, Header, TituloFuncao } from '../styles/global';
 import ModalSalvar from './ModalSalvar';
 import ModalErro from './ModalErro';
-import { Link } from "react-router-dom";
 
 export default function Listar(props: any) {
-  const funcao: any = props.funcao;
-  const colunas: any[] = props.colunas;
+  const { funcao, colunas } = props;
   const [listagem, setListagem] = useState([]);
   const [open, setOpen] = useState(false);
   const [erro, setErro] = useState(false);
-console.log(colunas);
-  useEffect(() => {
-    api.post("/" + funcao + "/listar", {
-    }).then((response) => setListagem(response.data));
-  }, [funcao]);
 
-  let listar: any = [];
+  useEffect(() => {
+    api.post(`/${funcao}/listar`, {}).then((response) => setListagem(response.data));
+  }, [funcao]);
 
   async function excluir(id: number): Promise<void> {
     const response = await api.post(`/${funcao}/excluir`, { id });
     setOpen(response.data);
     setErro(!response.data);
   }
-
-  let link: any = [];
-  listagem.forEach((element: any) => {
-    console.log(element);
-    link['editar'] = "/" + funcao + "/alterar/" + element.id;
-    link['listagem'] = "/" + funcao;
-
-    listar.push(
-      <tr>
-        {colunas.map((nomeColuna: any) => (
-          <td>{element[nomeColuna]}</td>
-        ))}
-        <td width={"20px"}>
-          <a href={link['editar']} className='editar' ><BsFillPencilFill /></a>
-        </td>
-        <td width={"20px"} onClick={() => excluir(element.id)} className='excluir'>
-          <FaTrashAlt></FaTrashAlt>
-        </td>
-      </tr>
-    )
-  });
 
   return (
     <Header>
@@ -59,22 +34,39 @@ console.log(colunas);
           <ModalErro funcao={funcao} />
         </Modal>
         <TituloFuncao>
-          <h3>
-            {funcao}
-          </h3>
-          <Link to={"adicionar"}>
+          <h3>{funcao}</h3>
+          <Link to="adicionar">
             <Adicionar>+</Adicionar>
           </Link>
         </TituloFuncao>
-        <table className='w-100'>
-          <tr>
-            {colunas.map((nome: any) => (
-              <th className='text-capitalize' >{nome.replaceAll('_', ' ')}</th>
+        <table className="w-100">
+          <thead>
+            <tr>
+              {colunas.map((nome: string) => (
+                <th className="text-capitalize" key={nome}>
+                  {nome.replaceAll('_', ' ')}
+                </th>
+              ))}
+              <th colSpan={2}></th>
+            </tr>
+          </thead>
+          <tbody>
+            {listagem.map((element: any) => (
+              <tr key={element.id}>
+                {colunas.map((nomeColuna: any) => (
+                  <td key={nomeColuna}>{element[nomeColuna]}</td>
+                ))}
+                <td width="20px">
+                  <a href={`/${funcao}/alterar/${element.id}`} className="editar">
+                    <BsFillPencilFill />
+                  </a>
+                </td>
+                <td width="20px" onClick={() => excluir(element.id)} className="excluir">
+                  <FaTrashAlt />
+                </td>
+              </tr>
             ))}
-            <th colSpan={2} ></th>
-          </tr>
-
-          {listar}
+          </tbody>
         </table>
       </Conteudo>
     </Header>
