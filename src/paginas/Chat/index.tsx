bab2d374c6f15fe6261beb599db9d5d6f577facd
gracione <gracione2017@gmail.com>
2023-07-23 14:03:54 -0300

    imagem ao perfil do usuario

--- src/paginas/Chat/index.tsx
+++ src/paginas/Chat/index.tsx
@@ -2,26 +2,33 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Header, Conteudo } from "../../styles/global";
 import api from "../../services/api";
+const defaultImgUrl = "./icons/sem_usuario.png";
 
 export default function Chat() {
   const [listagem, setListagem] = useState<any[]>([]);
 
   useEffect(() => {
-    api.post("/users/listar", {}).then((response) => {
+    api.post("/users/listar", {
+      id_usuario: localStorage.getItem("id_usuario")
+    }).then((response) => {
       setListagem(response.data);
     });
   }, []);
-
-  console.log(listagem);
 
   return (
     <Header>
       <Conteudo>
         <div className="d-flex flex-column">
-
           {listagem.map((usuario: any) => (
             <Link className="text-info" to={`/chat/${usuario.id}`} key={usuario.id}>
-              x  ----  {usuario.nome}
+              <img
+                className="rounded-circle col-1 h-25 border"
+                src={usuario.img_url || defaultImgUrl}
+                alt={usuario.nome}
+              />
+              <b className="p-2">
+              {usuario.nome}
+              </b>
             </Link>
           ))}
         </div>
