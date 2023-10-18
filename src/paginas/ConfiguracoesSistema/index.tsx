f27e5c308500c61e9ace78489a8f9132ec2ce51a
gracione <gracione2017@gmail.com>
2023-10-18 20:13:22 -0300

    intervalo de tempo

--- src/paginas/ConfiguracoesSistema/index.tsx
+++ src/paginas/ConfiguracoesSistema/index.tsx
@@ -11,20 +11,25 @@ export default function ConfiguracoesSistema() {
     api
       .post(`/configuracao-sistema/listar`, {})
       .then((response) => setListagem(response.data));
-    }, []);
+  }, []);
 
   return (
     <Header>
       <Conteudo>
         <h1>Configuração Sistema</h1>
 
-        <label>Frequencia</label>
-        <input
-          defaultValue={listagem['frequencia_horario']}
-          onChange={(e) => setFrequencia(e.target.value)}
-          required
-          className="form-control bg-white opacity-20" // Bootstrap classes for white background with 20% opacity
-        />
+        <div className="d-flex">
+          <label style={{ fontSize: '20px' }}  className="w-75 d-flex justify-content align-items-center">
+            Seleção de Intervalo de Tempo Personalizado :
+          </label>
+
+          <input
+            defaultValue={listagem['frequencia_horario']}
+            onChange={(e) => setFrequencia(e.target.value)}
+            required
+            className="w-25 form-control bg-white opacity-20" // Bootstrap classes for white background with 20% opacity
+          />
+        </div>
         <Alterar modulo="configuracao-sistema" dados={{ frequencia_horario: frequencia }} />
       </Conteudo>
     </Header>
